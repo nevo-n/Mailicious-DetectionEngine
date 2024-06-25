1a5b0f5a4e8a9cf60af019d41b5f2664fe8ce6bd
nevo-n <76394153+nevo-n@users.noreply.github.com>
2024-06-25 19:33:52 +0000

    Added Client Handler base code

---
+++ src/ClientHandler/api_server.py
@@ -0,0 +1,47 @@
+import os
+import json
+from flask import Flask, jsonify, request
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from src.DBHandler import DBHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+# read secret key from .env
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
+app = Flask("Mailicious")
+
+# Configure your JWT secret key
+app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
+jwt = JWTManager(app)
+
+def analyze_mail(customer_id, mail):
+  return True
+
+def is_customer(id):
+  return True
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    if not DBHandler().verify_login(username, password):
+      return jsonify({"msg": "Bad username or password"}), 401
+    
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+
+@app.route('/analyze', methods=['POST'])
+@jwt_required()
+def send_data_for_analysis():
+   customer_id = get_jwt_identity()
+   verdict = analyze_mail(customer_id, json.loads(request.data))
+   if verdict:
+      return jsonify({'verdict': f"{verdict}"}), 200
+   else:
+      return jsonify({'error': "internal"}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
